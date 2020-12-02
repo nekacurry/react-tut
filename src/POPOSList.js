9ce0e81e79fde1d930a782885da4f029dba2234a
nekacurry <anneka498@gmail.com>
2020-12-02 15:08:00 -0600

    dynamic content and footer

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -3,41 +3,25 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
-
+import data from './sfpopos-data.json'
 
 function POPOSList() {
+
+  const spaces = data.map(( { title, address, images, hours } ) => {
+    return (
+      <POPOSSpace
+        key={title} // The title could be a key
+        name={title}
+        address={address}
+        image={images[0]}
+        hours={hours}
+      />
+    )
+  })
+
   return (
     <div className="POPOSList">
-      <POPOSSpace
-        name="50 California St."
-        address="50 California St."
-        image="50-california-st.jpg"
-      />
-      <POPOSSpace 
-        name="100 Pine St."
-        address="100 Pine St."
-        image="100-pine.jpg"
-      />
-      <POPOSSpace 
-        name="101 California St."
-        address="101 California St."
-        image="101-california.jpg"
-      />
-      <POPOSSpace 
-        name="343 Sansome Roof Garden"
-        address="343 Sansome Ave."
-        image="343-sansome-roof-garden.jpg"
-      />
-      <POPOSSpace 
-        name="555 California St."
-        address="555 California St."
-        image="555-california.jpg"
-      />
-      <POPOSSpace
-        name="Citigroup Center"
-        address="346 Plaza Rd."
-        image="citigroup-center.jpg"
-      />
+      { spaces }
     </div>
   )
 }
